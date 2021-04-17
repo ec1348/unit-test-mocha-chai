d7a0b7f0e1c65fe5fd9d4ec6dced19afc994eb13
Ming Jhang Chen <ec1348@gmail.com>
2021-04-18 00:57:41 +0800

    Test mocha

---
+++ test/main_text.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+
+//Test suite
+describe('mocha', function() {
+    //Test spec (Unit test)
+    it('should run our test using npm', function(){
+        expect(true).to.be.ok;
+    })
+})
