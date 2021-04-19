7a798d992dd27b6a6a08e7fe2b68a101d37f316c
Ming Jhang Chen <ec1348@gmail.com>
2021-04-19 12:49:27 +0800

    Check FOr ship

---
+++ test/ship_test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('checkForShip', function(){
+    var checkForShip = require('../game_logic/ship_methods').checkForShip;
+
+    it('should correctly report no ship at a given players coordinate', function (){
+
+    });
+});
